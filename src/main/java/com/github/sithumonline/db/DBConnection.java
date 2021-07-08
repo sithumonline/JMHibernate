7f9814193cdcbc8639d0fbcbcdde262779392b37
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-07-08 06:50:24 +0530

    Add properties file

--- src/main/java/com/github/sithumonline/db/DBConnection.java
+++ src/main/java/com/github/sithumonline/db/DBConnection.java
@@ -1,22 +1,38 @@
 package com.github.sithumonline.db;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public class DBConnection {
     private Connection conn;
     private static DBConnection dbConnection;
 
     private DBConnection() {
-        String dbURL = "jdbc:mysql://localhost:3306/javadb?useSSL=false";
-        String username = "root";
-        String password = "mysespw";
+        try (InputStream input = DBConnection.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
 
-        try {
-            conn = DriverManager.getConnection(dbURL, username, password);
-        } catch (SQLException ex) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+
+            prop.load(input);
+
+            String dbURL = prop.getProperty("db.url");
+            String username = prop.getProperty("db.username");
+            String password = prop.getProperty("db.password");
+
+            try {
+                conn = DriverManager.getConnection(dbURL, username, password);
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
