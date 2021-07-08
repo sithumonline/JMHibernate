f3ec242308fd6e9cf79a26a8774f8e3bf6a519df
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-07-08 23:23:06 +0530

    Clean up

--- src/main/java/com/github/sithumonline/controller/UserController.java
+++ src/main/java/com/github/sithumonline/controller/UserController.java
@@ -9,7 +9,6 @@ public class UserController {
 
     public static boolean addUser(Users userDTO)throws Exception{
         UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
         return userService.addUser(userDTO);
     }
 
@@ -17,11 +16,6 @@ public class UserController {
         UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
         return userService.updateUser(userDTO);
     }
-
-//    public static Users getUserById(String userId)throws Exception{
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        return userService.getByUserId(userId);
-//    }
 
     public static boolean deleteUser(String userId)throws Exception{
         UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
@@ -32,25 +26,5 @@ public class UserController {
         UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
         return userService.getAllUsers();
     }
-
-//    public static boolean addUserObserver(UserObserver userObserver)throws Exception{
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        return userService.addUserObserver(userObserver);
-//    }
-//
-//    public static boolean removeUserObserver(UserObserver userObserver)throws Exception{
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        return userService.removeUserObserver(userObserver);
-//    }
-//
-//    public static boolean reserveUser(String userId)throws Exception{
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        return userService.reserve(userId);
-//    }
-//
-//    public static boolean releaseUser(String userId)throws Exception{
-//        UserService userService = (UserService)  BOFactory.getInstance().getBOFactory(BOFactory.BOTypes.USER);
-//        return userService.release(userId);
-//    }
     
 }
