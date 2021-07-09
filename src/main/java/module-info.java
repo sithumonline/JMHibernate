5c3754be0fd7d8d38bc9f756327936a71a46b62c
Sithum Bopitiya <sithumsandeepap@gmail.com>
2021-07-09 10:38:23 +0530

    Fix mess

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,7 +9,7 @@ module com.github.sithumonline {
 
     opens com.github.sithumonline to javafx.fxml;
     opens com.github.sithumonline.view.handler to javafx.fxml;
-//    opens com.github.sithumonline.model to javafx.base;
     exports com.github.sithumonline;
     exports com.github.sithumonline.view.handler;
+    exports com.github.sithumonline.entity;
 }
